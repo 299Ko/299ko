3fa34761b4842e7063f0901b96e2986e485c8af5
MaxenceCauderlier <mx.koder@gmail.com>
2022-10-26 19:20:02 +0200

    [MAJ] Update Font-Awesome to 6.2

--- common/config.php
+++ common/config.php
@@ -18,7 +18,7 @@ define('DATA_PLUGIN', ROOT . 'data/plugin/');
 define('THEMES', ROOT . 'theme/');
 define('PLUGINS', ROOT . 'plugin/');
 define('ADMIN_PATH', ROOT . 'admin/');
-define('FONTICON', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css');
+define('FONTICON', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css');
 define('FANCYCSS', "https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css");
 define("FANCYJS", "https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js");
 if (file_exists(DATA . 'key.php'))
